5f2174b4db7b25dc98ffe05a7071260e6064c43a
believeinlain <saaelmore@gmail.com>
2020-10-29 14:32:40 -1000

    Optimized Arc* a bit

--- PyAedatTools/ArcStar.py
+++ PyAedatTools/ArcStar.py
@@ -39,9 +39,6 @@ def createCircleMask(radius, thickness=2):
 CircleMask3 = createCircleMask(3, 2)
 CircleMask4 = createCircleMask(4, 3) # thickness 3 leaves gaps but thickness 4 overlaps
 
-#print([(c[0]+3,c[1]+3) for c in CircleMask3])
-#print([(c[0]+4,c[1]+4) for c in CircleMask4])
-
 # initialize 2d array of zero tuples where each tuple is (tr, tl)
 def getInitialSAE(width, height):
     return [[(0, 0) for i in range(height)] for j in range(width)]
@@ -52,7 +49,6 @@ def updateSAE(SAE, eventData, i, polarity, width, height, k=50):
     x = eventData['x'][i]
     y = eventData['y'][i]
     ePolarity = eventData['polarity'][i]
-
     startTime = eventData['timeStamp'][0]
 
     # get event time relative to time 0
@@ -67,7 +63,6 @@ def updateSAE(SAE, eventData, i, polarity, width, height, k=50):
         SAE[x][y] = (SAE[x][y][0], t)
 
 # determine if a new event is a corner
-# TODO: this needs a lot of optimization, way too many for loops
 def isEventCorner(SAE, eX, eY, circleRadius=3):
     # min and max arclengths to detect a corner
     Lmin = circleRadius
@@ -81,66 +76,51 @@ def isEventCorner(SAE, eX, eY, circleRadius=3):
     else:
         circleMask = createCircleMask(circleRadius)
 
-    # get circle coordinates
-    Circle = [(eX+m[0], eY+m[1]) for m in circleMask]
-
     # get SAE dimensions
     width = len(SAE)
     height = len(SAE[0])
 
-    # if circle is off the edge just quit
-    # TODO: maybe handle this better?
-    for c in Circle:
-        if c[0] < 0 or c[0] > width:
-            return False
-        if c[1] < 0 or c[1] > height:
-            return False
+    # get circle coordinates within bounds of SAE
+    Circle = [(eX+m[0], eY+m[1]) for m in circleMask if \
+        eX+m[0]>=0 and eX+m[0]<=width and eY+m[1]>=0 and eY+m[1]<=height]
 
     # Initialize Anew to the newest event on the circle (by tr)
-    newestIndex = 0
-    newestTime = 0
-    for i in range(len(Circle)):
-        t = getAgeOfCircleElement(SAE, Circle, i)
-        if t > newestTime:
-            newestIndex = i
-            newestTime = t
+    newestIndex = max({getAgeOfCircleElement(SAE, Circle[i]):i for i in range(len(Circle))}.items())[1]
     
     Anew = [newestIndex]
     AnewOldestElement = newestIndex
+    AnewOldestElementAge = getAgeOfCircleElement(SAE, Circle[AnewOldestElement])
     
     # initialize CW and CCW points
-    CWIndex = newestIndex+1
-    CCWIndex = newestIndex-1
-    # wrap indices
-    if CWIndex == len(Circle):
-        CWIndex = 0
-    if CCWIndex < 0:
-        CCWIndex = len(Circle)-1
+    CWIndex = NextElementCW(Circle, newestIndex)
+    CCWIndex = NextElementCCW(Circle, newestIndex)
     
+    # determine arcs to find corners
     while CCWIndex != CWIndex:
-        if getAgeOfCircleElement(SAE, Circle, CWIndex) \
-            > getAgeOfCircleElement(SAE, Circle, CCWIndex):
-            if getAgeOfCircleElement(SAE, Circle, AnewOldestElement) \
-                <= getAgeOfCircleElement(SAE, Circle, CWIndex) \
-                or len(Anew) < Lmin:
-                    ExpandUntilElementCW(Anew, Circle, CWIndex)
-                    AnewOldestElement = GetOldestElement(SAE, Anew, Circle)
+        CWIndexAge = getAgeOfCircleElement(SAE, Circle[CWIndex])
+        CCWIndexAge = getAgeOfCircleElement(SAE, Circle[CCWIndex])
+
+        if CWIndexAge > CCWIndexAge:
+            if AnewOldestElementAge <= CWIndexAge or len(Anew) < Lmin:
+                ExpandUntilElementCW(Anew, Circle, CWIndex)
+                AnewOldestElement = GetOldestElement(SAE, Anew, Circle)
+                AnewOldestElementAge = getAgeOfCircleElement(SAE, Circle[AnewOldestElement])
                     
             CWIndex = NextElementCW(Circle, CWIndex)
         else:
-            if getAgeOfCircleElement(SAE, Circle, AnewOldestElement) \
-                <= getAgeOfCircleElement(SAE, Circle, CCWIndex) \
-                or len(Anew) < Lmin:
-                    ExpandUntilElementCCW(Anew, Circle, CCWIndex)
-                    AnewOldestElement = GetOldestElement(SAE, Anew, Circle)
+            if AnewOldestElementAge <= CCWIndexAge or len(Anew) < Lmin:
+                ExpandUntilElementCCW(Anew, Circle, CCWIndex)
+                AnewOldestElement = GetOldestElement(SAE, Anew, Circle)
+                AnewOldestElementAge = getAgeOfCircleElement(SAE, Circle[AnewOldestElement])
 
             CCWIndex = NextElementCCW(Circle, CCWIndex)
     
     # if len(Anew) is between Lmin and Lmax
     # or its complementary arc is between Lmin and Lmax
     # return true, else return false
-    if (len(Anew) >= Lmin and len(Anew) <= Lmax) or \
-        (len(Circle)-len(Anew) >= Lmin and len(Circle)-len(Anew) <=Lmax):
+    arcLen = len(Anew)
+    arcLenC = len(Circle)-len(Anew)
+    if (arcLen >= Lmin and arcLen <= Lmax) or (arcLenC >= Lmin and arcLenC <=Lmax):
         return True
     else:
         return False
@@ -160,9 +140,8 @@ def NextElementCCW(Circle, Index):
     return next
 
 # helper function to make code more readable
-def getAgeOfCircleElement(SAE, Circle, Index):
-    #print(Circle[Index][0], Circle[Index][1])
-    return SAE[Circle[Index][0]][Circle[Index][1]][0]
+def getAgeOfCircleElement(SAE, circleElement):
+    return SAE[circleElement[0]][circleElement[1]][0]
 
 # expand Anew to Index
 def ExpandUntilElementCW(Anew, Circle, Index):
@@ -179,11 +158,6 @@ def ExpandUntilElementCCW(Anew, Circle, Index):
 
 # return new oldest element in Anew
 def GetOldestElement(SAE, Anew, Circle):
-    oldestIndex = 0
-    oldestTime = 0
-    for i in Anew:
-        t = getAgeOfCircleElement(SAE, Circle, i)
-        if t < oldestTime:
-            oldestIndex = i
-            oldestTime = t
-    return oldestIndex
+    # create a dict of timeStamp:index pairs for each element in Anew
+    # then find the min timeStamp and return the associated index
+    return min({getAgeOfCircleElement(SAE, Circle[i]):i for i in Anew}.items())[1]
