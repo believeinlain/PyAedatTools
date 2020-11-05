b781d43a36dc2813d2e4f24212eabae62a542568
believeinlain <saaelmore@gmail.com>
2020-11-05 10:33:24 -1000

    Added a bit more

--- ClusterTracking.py
+++ ClusterTracking.py
@@ -1,3 +1,14 @@
 
 class Cluster:
-    def __init__(x, y, )
+    def __init__(self, x, y, events):
+        self.x = x
+        self.y = y
+        self.events = events
+
+class ClusterTracker:
+    def __init__(self):
+        self.clusters = []
+        self.eventBuffer = []
+        self.oldestEvent
+
+    def 
